5d481997b130ea83ce3b0446357e84b47af4894d
dosmak <dosmak@erietecinc.com>
2018-04-09 09:55:59 -0400

    Replaced CherryPy with HTTPD

--- resources/lib/webservice.py
+++ resources/lib/webservice.py
@@ -1,17 +1,25 @@
 import threading
-import cherrypy
+import thread
 import xbmc, xbmcgui, xbmcaddon
 import requests, urllib
 import cookielib
+import Cookie
 import os
 import sys
-import m3u8
+import base64
+import re
+import time
+import traceback
+import socket
+from SocketServer import ThreadingMixIn
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from urllib import *
 
 ADDON = xbmcaddon.Addon()
 PS_VUE_ADDON = xbmcaddon.Addon('plugin.video.psvue')
 ADDON_PATH_PROFILE = xbmc.translatePath(PS_VUE_ADDON.getAddonInfo('profile'))
 UA_ANDROID_TV = 'Mozilla/5.0 (Linux; Android 6.0.1; Hub Build/MHC19J; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Safari/537.36'
-VERIFY = True
+VERIFY = False
 
 
 def load_cookies():
@@ -25,7 +33,7 @@ def load_cookies():
     return cj
 
 
-def get_master(url):
+def epg_get_stream(url):
     headers = {
         'Accept': '*/*',
         'Content-type': 'application/x-www-form-urlencoded',
@@ -41,120 +49,150 @@ def get_master(url):
     }
 
     r = requests.get(url, headers=headers, cookies=load_cookies(), verify=VERIFY)
-    stream_url = r.json()['body']['video']
+    json_source = r.json()
+    stream_url = json_source['body']['video']
 
     return stream_url
 
 
-def play_epg_as_pvr(stream_url):
-    headers = {
-        'Accept': '*/*',
-        'Content-type': 'application/x-www-form-urlencoded',
-        'Origin': 'https://vue.playstation.com',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Referer': 'https://vue.playstation.com/watch/live',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'User-Agent': UA_ANDROID_TV,
-        'Connection': 'Keep-Alive',
-        'Host': 'media-framework.totsuko.tv',
-        'reqPayload': PS_VUE_ADDON.getSetting(id='EPGreqPayload'),
-        'X-Requested-With': 'com.snei.vue.android'
-    }
-    r = requests.get(stream_url, headers=headers)
-    variant_m3u8 = m3u8.loads(r.text)
-    bandwidth = 0
-    best_stream = ''
-    for playlist in variant_m3u8.playlists:
-        xbmc.log(str(playlist.stream_info.bandwidth))
-        xbmc.log(playlist.uri)
-        if playlist.stream_info.bandwidth > bandwidth:
-            bandwidth = playlist.stream_info.bandwidth
-            best_stream = playlist.uri
-
-    if 'http' not in best_stream and best_stream != '':
-        stream_url = stream_url.replace(stream_url.rsplit('/', 1)[-1], best_stream)
-
-    return stream_url
-
-
-def play_epg_as_listitem(stream_url):
-
-    headers = '|User-Agent='
-    headers += 'Adobe Primetime/1.4 Dalvik/2.1.0 (Linux; U; Android 6.0.1 Build/MOB31H)'
-    headers += '&Cookie=reqPayload=' + urllib.quote('"' + PS_VUE_ADDON.getSetting(id='EPGreqPayload') + '"')
-    listitem = xbmcgui.ListItem()
-    listitem.setMimeType("application/x-mpegURL")
-    if xbmc.getCondVisibility('System.HasAddon(inputstream.adaptive)'):
-        listitem.setProperty('inputstreamaddon', 'inputstream.adaptive')
-        listitem.setProperty('inputstream.adaptive.manifest_type', 'hls')
-        listitem.setProperty('inputstream.adaptive.stream_headers', headers)
-        listitem.setProperty('inputstream.adaptive.license_key', headers)
+def find(source, start_str, end_str):
+    start = source.find(start_str)
+    end = source.find(end_str, start + len(start_str))
+    if start != -1:
+        return source[start + len(start_str):end]
     else:
-        stream_url += headers
-
-    listitem.setPath(stream_url)
-    xbmc.Player().play(item=stream_url+headers, listitem=listitem)
+        return ''
 
 
-class Root(object):
-    exposed = True
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(server):
+        xbmc.log("WebServer: Get request Received")
 
-    @cherrypy.expose
-    def GET(self, params):
-        master_url = get_master(params)
+        ##########################################################################################
+        # Stream preparation chunk
+        ##########################################################################################
 
-        if '18.' in xbmc.getInfoLabel("System.BuildVersion"):
-            cherrypy.response.cookie['reqPayload'] = PS_VUE_ADDON.getSetting(id='EPGreqPayload')
-            cherrypy.response.cookie['reqPayload']['Domain'] = 'totsuko.tv'
-            cherrypy.response.cookie['reqPayload']['Path'] = '/'
-            stream_url = play_epg_as_pvr(master_url)
-            raise cherrypy.HTTPRedirect(stream_url)
-        else:
-            cherrypy.response.status = 302
-            cherrypy.response.headers['Connection'] = 'keep-alive'
-            cherrypy.response.headers['Content-Type'] = 'text/html'
-            # Play default mp4 to avoid playback failed dialog
-            cherrypy.response.headers['Location'] = 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
-            play_epg_as_listitem(master_url)
+        # Extract channel url from request URI
+        # channel_url = server.path[(server.path.find('params=') + 15):]
+        channel_url = find(server.path, 'params=', '.m3u8')
+        channel_url = urllib.unquote(channel_url)
+        xbmc.log("Received Channel URL: " + channel_url)
+
+        # Retrieve stream master file url for channel
+        stream_url = epg_get_stream(channel_url)
+        xbmc.log("Retrieved Stream URL: " + stream_url)
+
+        xbmc.log("Retrieving Master File")
+        response = requests.get(stream_url, headers="", cookies=load_cookies(), verify=VERIFY)
+        response_code = response.status_code
+        response_headers = response.headers
+        response_content = response.text
+
+        # Inject master file HOST into stream URLs
+        master_file = ''
+        last_stream = ''
+        line = re.compile("(.+?)\n").findall(response_content)
+        for temp in line:
+            if '#EXT' not in temp:
+                if 'http' not in temp:
+                    temp = stream_url.replace(stream_url.rsplit('/', 1)[-1], temp)
+                    last_stream = temp
+            master_file += temp + '\n'
+        xbmc.log('New Master File: ' + master_file)
+
+        ##########################################################################################
+        # Request response chunk
+        ##########################################################################################
+
+        # Response code for the get request
+        # 200 OK
+        # 202 Accepted
+        # 301 Moved Permanently
+        # 302 Found
+        # 303 See Other
+        # 308 Permanent Redirect
+        server.send_response(303)
+
+        # Header array for the response, toggle Connection Close or Keep/Alive depending on the reponse code
+        headers = {
+            'Content-type': 'text/html;charset=utf-8',
+            'Connection': 'close',
+            'Host': 'media-framework.totsuko.tv',
+            'Location': last_stream,
+            'Set-Cookie': 'reqPayload=' + PS_VUE_ADDON.getSetting(id='EPGreqPayload') + '; Domain=totsuko.tv; Path=/'
+        }
+
+        # Loop through the Header Array sending each one individually
+        for key in headers:
+            try:
+                value = headers[key]
+                server.send_header(key, value)
+            except Exception, e:
+                xbmc.log(e)
+                pass
+
+        # Tells the server the headers are done and the body can be started
+        server.end_headers()
+
+        # Write body content to the response
+        # server.wfile.write(master_file)
+
+        # Close the server response file
+        server.wfile.close()
+
+
+class Server(HTTPServer):
+    def get_request(self):
+        self.socket.settimeout(5.0)
+        result = None
+        while result is None:
+            try:
+                result = self.socket.accept()
+            except socket.timeout:
+                pass
+        result[0].settimeout(1000)
+        return result
+
+
+class ThreadedHTTPServer(ThreadingMixIn, Server):
+    """Handle requests in a separate thread."""
 
 
 class PSVueWebService(threading.Thread):
-    __root = None
+    httpd = None
+    hostname = '127.0.0.1'
+    port = 54321
 
     def __init__(self):
-        self.__root = Root()
 
         if ADDON.getSetting(id='port') == '':
             dialog = xbmcgui.Dialog()
-            dialog.notification('PS Vue EPG', 'Please enter a port number in the PS Vue EPG Build Settings', xbmcgui.NOTIFICATION_INFO, 5000, False)
+            dialog.notification('PS Vue EPG', 'Please enter a port number in the PS Vue EPG Build Settings',
+                                xbmcgui.NOTIFICATION_INFO, 5000, False)
             sys.exit()
         else:
-            port = ADDON.getSetting(id='port')
+            self.port = ADDON.getSetting(id='port')
 
-        cherrypy.config.update({
-            'server.socket_host': '127.0.0.1',
-            'server.socket_port': int(port),
-            'engine.timeout_monitor.frequency': 5,
-            'server.shutdown_timeout': 1
-        })
+        if self.httpd == None:
+            socket.setdefaulttimeout(10)
+            server_class = ThreadedHTTPServer
+            xbmc.log('Initialized WebServer Hostname | Port -> ' + self.hostname + ' | ' + str(self.port))
+            self.httpd = server_class((self.hostname, int(self.port)), RequestHandler)
+        else:
+            self.httpd.handle_request()
+
         threading.Thread.__init__(self)
 
     def run(self):
-        cherrypy.tree.mount(Root(), '/psvue', {
-        '/': {
-
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-
-            'tools.sessions.on': False,
-
-            'tools.response_headers.on': True
-
-            }
-
-        })
-        cherrypy.engine.start()
+        xbmc.log("WebServer Started - %s:%s" % (self.hostname, self.port))
+        self.httpd.serve_forever()
+        self.httpd.handle_request()
 
     def stop(self):
-        cherrypy.engine.exit()
+        try:
+            self.httpd.server_close()
+            xbmc.log("WebServer Stopped %s:%s" % (self.hostname, self.port))
+        except:
+            pass
+
         self.join(0)
-        del self.__root
